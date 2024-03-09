a49aa1da00ff92f47d09ab935a5251f93221011c
Kylan Roberson <83306192+kroberso-intc@users.noreply.github.com>
2024-03-09 09:57:39 -0800

    [Fix] Only compile mem_ss with INCLUDE_DDR4 macro (#1328)

--- ipss/mem/mem_design_files.tcl
+++ ipss/mem/mem_design_files.tcl
@@ -1,13 +1,24 @@
 # Copyright (C) 2023 Intel Corporation.
 # SPDX-License-Identifier: MIT
 
-# MemSS IP
-set_global_assignment -name IP_FILE $::env(BUILD_ROOT_REL)/ipss/mem/qip/mem_ss/mem_ss.ip
-# Used only in simulation. Loading it here adds ed_sim_mem to the simulation environment.
-# It is not instantiated on HW.
-set_global_assignment -name IP_FILE $::env(BUILD_ROOT_REL)/ipss/mem/qip/ed_sim/ed_sim_mem.ip
+set vlog_macros [get_all_global_assignments -name VERILOG_MACRO]
+set include_mss 0
 
-# Add the Memory Subsystem to the dictionary of IP files that will be parsed by OFS
-# into the project's ofs_ip_cfg_db directory. Parameters from the configured
-# IP will be turned into Verilog macros.
-dict set ::ofs_ip_cfg_db::ip_db $::env(BUILD_ROOT_REL)/ipss/mem/qip/mem_ss/mem_ss.ip [list mem_ss mem_ss_get_cfg.tcl]
+foreach_in_collection m $vlog_macros {
+    if { [string equal "INCLUDE_DDR4" [lindex $m 2]] } {
+        set include_mss 1
+    }
+}
+
+if {$include_mss == 1} {
+    # MemSS IP
+    set_global_assignment -name IP_FILE $::env(BUILD_ROOT_REL)/ipss/mem/qip/mem_ss/mem_ss.ip
+    # Used only in simulation. Loading it here adds ed_sim_mem to the simulation environment.
+    # It is not instantiated on HW.
+    set_global_assignment -name IP_FILE $::env(BUILD_ROOT_REL)/ipss/mem/qip/ed_sim/ed_sim_mem.ip
+
+    # Add the Memory Subsystem to the dictionary of IP files that will be parsed by OFS
+    # into the project's ofs_ip_cfg_db directory. Parameters from the configured
+    # IP will be turned into Verilog macros.
+    dict set ::ofs_ip_cfg_db::ip_db $::env(BUILD_ROOT_REL)/ipss/mem/qip/mem_ss/mem_ss.ip [list mem_ss mem_ss_get_cfg.tcl]
+}
