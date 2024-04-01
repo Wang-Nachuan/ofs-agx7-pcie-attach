686ff619ea36914fcaf5ef757bb12d341cffb8b2
sravanirudraraju <101619835+sravanirudraraju@users.noreply.github.com>
2024-04-02 01:30:51 +0530

    Srudrarx/qsfp pmci build errors (#1361)
    * for F2000X, fixed the build error
    
    * Updated rtl_pcie.f file
    
    * updated msim_filelist.sh

--- sim/scripts/rtl_pcie.f
+++ sim/scripts/rtl_pcie.f
@@ -4,3 +4,4 @@
 $OFS_ROOTDIR/sim/bfm/ofs_axis_bfm/pcie_top.sv
 $OFS_ROOTDIR/sim/bfm/ofs_axis_bfm/pcie_wrapper.sv
 $OFS_ROOTDIR/sim/bfm/ofs_axis_bfm/axi_s_adapter.sv
+$OFS_ROOTDIR/ofs-common/src/fpga_family/agilex/pcie_ss/pcie_csr.sv
